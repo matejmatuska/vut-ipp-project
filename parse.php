83b53df913b84c25ba09c9a1e34a059051426b78
Matej Matuska <75834032+matejmatuska@users.noreply.github.com>
2022-02-27 21:21:14 +0100

    Parser: Add code docs

--- parse.php
+++ parse.php
@@ -1,11 +1,12 @@
 <?php
+// TODO stderr
 define("RESULT_OK", 0);
 define("RESULT_ERR_MISSING_ARG", 10);
 define("RESULT_ERR_INVALID_ARGS", 10);
 define("RESULT_ERR_OPENING_IN_FILE", 11);
 define("RESULT_ERR_OPENING_OUT_FILE", 12);
 
-// 20-69 navratove kody chyb specifickych pro jednotlive skripty
+// exit codes reserved for parser
 define("RESULT_ERR_INVALID_HEADER", 21);
 define("RESULT_ERR_MISSING_HEADER", 21);
 define("RESULT_ERR_INVALID_OPCODE", 22);
@@ -17,14 +18,6 @@ define("RESULT_ERR_INTERNAL", 99);
 
 ini_set('display_errors', 'stderr');
 
-function strip_comment($line) {
-    $index = strpos($line, "#");
-    if ($index === false) {
-        return $line;
-    }
-    return substr($line, 0, $index);
-}
-
 if ($argc == 2 && strcmp("--help", $argv[1]) == 0) {
     echo "Usage: parse|parse --help\n";
     exit(RESULT_OK);
@@ -34,54 +27,116 @@ else if ($argc >= 2) {
     exit(RESULT_ERR_INVALID_ARGS);
 }
 
+/**
+ * Returns string with trailing comment stripped
+ * If line contains just comment returns empty string
+ */
+function strip_comment($line) {
+    $index = strpos($line, "#");
+    if ($index === false) {
+        return $line;
+    }
+    return substr($line, 0, $index);
+}
+
+/**
+ * Checks whether string is a valid variable identifier, including frame specification
+ */
 function check_var($var) {
     return preg_match("/^[GLT]F@[a-zA-Z_\-$&%*!?][\w\-$&%*!?]*$/", $var);
 }
 
+/**
+ * Checks whether string is a valid label name
+ */
 function check_label($label) {
     return preg_match("/^[a-zA-Z_\-$&%*!?][\w\-$&%*!?]*$/", $label);
 }
 
+/**
+ * Checks whether string is a valid type
+ *
+ * Valid types are "string", "int", "bool" and "nil"
+ */
 function check_type($type) {
     return $type == "string" || $type == "int" || $type == "bool" || $type == "nil";
 }
 
+/**
+ * Checks whether string is a valid constant, including type definition
+ */
 function check_const($const) {
     return preg_match("/^int@[+-]?\d+$/", $const)
-        //|| preg_match("/^string@[\x{000}-\x{3E7}]*$/u", $const)
         || preg_match('/^string@(\\\\\d\d\d|[^\x00-\x20\x23\x5C])*$/u', $const)
         || $const == "bool@true" || $const == "bool@false"
         || $const == "nil@nil";
 }
 
+/**
+ * Checks whether string is a valid symbol
+ *
+ * Symbol is either a variable or constant
+ */
 function check_symb($symb) {
     // const or var
     return (check_var($symb) || check_const($symb));
 }
 
+/**
+ * Converts reserved XML characters in string to their correct XML representation
+ *
+ * For example "<" is converted to "&lt"
+ */
 function xml_convert_special_chars($string) {
     $patterns = array("/&/", "/</", "/>/");
     $replacements = array("&amp;", "&lt;", "&gt;");
     return preg_replace($patterns, $replacements, $string);
 }
 
+/**
+ * Generates a child XML element for a variable argument in the specified parent
+ *
+ * @param instr_xml parent XML element
+ * @param index argument index
+ * @param value variable name including frame specifier
+ */
 function xml_gen_var($instr_xml, $index, $value) {
     $value = xml_convert_special_chars($value);
-    //echo "here: ".$value."\n";
     $arg1 = $instr_xml->addChild("arg".$index + 1, $value);
     $arg1->addAttribute("type", "var");
 }
 
+/**
+ * Generates a child XML element for a label argument in the specified parent
+ *
+ * @param instr_xml parent XML element
+ * @param index argument index
+ * @param value label name
+ */
 function xml_gen_label($instr_xml, $index, $value) {
     $arg1 = $instr_xml->addChild("arg".$index + 1, $value);
     $arg1->addAttribute("type", "label");
 }
 
+/**
+ * Generates a child XML element for a type argument in the specified parent
+ *
+ * @param instr_xml parent XML element
+ * @param index argument index
+ * @param value type
+ */
 function xml_gen_type($instr_xml, $index, $value) {
     $arg1 = $instr_xml->addChild("arg".$index + 1, $value);
     $arg1->addAttribute("type", "type");
 }
 
+/**
+ * Generates a child XML element for a constant argument in the specified parent
+ *
+ * @param instr_xml parent XML element
+ * @param index argument index
+ * @param value constant value including type specifier
+ */
 function xml_gen_const($instr_xml, $index, $value) {
     $type_and_val = explode('@', $value, 2);
     if ($type_and_val[1] == "string") {
@@ -91,6 +146,13 @@ function xml_gen_const($instr_xml, $index, $value) {
     $arg1->addAttribute("type", $type_and_val[0]);
 }
 
+/**
+ * Generates a child XML element for a symbol argument in the specified parent
+ *
+ * @param instr_xml parent XML element
+ * @param index argument index
+ * @param value variable or constant
+ */
 function xml_gen_symb($instr_xml, $index, $value) {
     // const or var
     if (check_var($value)) {
@@ -108,6 +170,14 @@ $xml = new SimpleXMLElement(
     '<?xml version="1.0" encoding="UTF-8"?><program></program>');
 $xml->addAttribute('language', 'IPPcode22');
 
+/**
+ * Parses instruction and it's arguments or exits the script with appropriate error code
+ *
+ * @param opcode instruction opcode
+ * @param args instructiom arguments
+ * @param xml_parent parent XML element used for generating instruction and arg child elements
+ * @param order instruction order
+ */
 function parse_instr($opcode, $args, $xml_parent, $order) {
     $instr = $xml_parent->addChild('instruction');
     $instr->addAttribute('order', $order);
@@ -158,7 +228,7 @@ function parse_instr($opcode, $args, $xml_parent, $order) {
             xml_gen_label($instr, 0, $args[0]);
             break;
 
-        // label sym0 sym2
+        // label sym1 sym2
         case "JUMPIFEQ":
         case "JUMPIFNEQ":
             if (count($args) != 3)
@@ -189,7 +259,6 @@ function parse_instr($opcode, $args, $xml_parent, $order) {
 
             xml_gen_var($instr, 0, $args[0]);
             xml_gen_type($instr, 1, $args[1]);
-            //TODO type
             break;
 
         // symb
@@ -217,7 +286,7 @@ function parse_instr($opcode, $args, $xml_parent, $order) {
                 exit(RESULT_ERR_OTHER_SYNTAX);
             break;
 
-        // var sym0 sym2
+        // var sym1 sym2
         case "ADD":
         case "SUB":
         case "MUL":
@@ -253,6 +322,7 @@ function parse_instr($opcode, $args, $xml_parent, $order) {
 }
 
 $order = 1;
+// parse all instructions
 while ($line = fgets(STDIN)) {
     $line = strip_comment($line);
     $line = trim($line);
@@ -265,18 +335,18 @@ while ($line = fgets(STDIN)) {
             $has_header = true;
             continue;
         } else {
-            echo "here line: ".$line."\n";
+            echo "Missing file header\n";
             exit(RESULT_ERR_MISSING_HEADER);
         }
     }
 
-    //echo ($line)."\n";
     $split = preg_split("/\s+/", $line);
     $opcode = strtoupper(array_shift($split));
 
     parse_instr($opcode, $split, $xml, $order++);
 }
-//TODO sanitize < > &
+
+// format the constructed XML and print it to stdout
 $dom = new DOMDocument('1.0');
 $dom->preserveWhiteSpace = false;
 $dom->formatOutput = true;
